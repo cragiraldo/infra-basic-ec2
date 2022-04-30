5264b5f54b51943288faa93cb07430b41b2a4c72
cgiraldo <cgiraldo@lendingfront.com>
2022-04-30 11:33:50 -0500

    define sg

--- resources/ec2/security-group/main.tf
+++ resources/ec2/security-group/main.tf
@@ -4,5 +4,12 @@ module "web_server_sg" {
   name        = "web-server"
   description = "Security group for web-server with HTTP ports open within VPC"
   vpc_id      = var.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port = 80
+      to_port   = 80
+      protocol  = "tcp"
+      description = "user service"
+      cidr_blocks = "0.0.0.0/0"
+    }
 }
