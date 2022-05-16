045a95a9d116fe9b71ee8580009fa452707e2697
cgiraldo <cgiraldo@lendingfront.com>
2022-05-16 05:51:20 -0500

    Configure SG ssh

--- resources/ec2/security-group/main.tf
+++ resources/ec2/security-group/main.tf
@@ -11,13 +11,13 @@ module "web_server_sg" {
       protocol  = "tcp"
       description = "user service"
       cidr_blocks = "0.0.0.0/0"
-    }, 
+    },
     {
       from_port = 22
       to_port   = 22
       protocol  = "tcp"
       description = "ssh admin"
       cidr_blocks = "0.0.0.0/0"
-    }
+    },
   ]
 }
